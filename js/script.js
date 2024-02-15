98ba5d4d1a3c6f99b2fc8f3f8e4d8dd172796343
akradadiya28 <akradadiya28@gmail.com>
2024-02-15 12:01:10 +0530

    chamges

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-$(document).ready(function(){
-   $(".drop").dblclick(function(){
-      $(".drop-down").slideToggle();
-      $(".arrow").delay(2000).addClass("rotate");x
-   })
+$(document).ready(function () {
+
 })
