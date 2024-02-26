b75cfb07e152e9622ad6fc33eebd80707062e866
akradadiya28 <akradadiya28@gmail.com>
2024-02-26 12:26:48 +0530

    chart added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,34 @@ $(document).ready(function () {
         $("body").toggleClass("close");
     })
 })
+// chart bar
+const ctx = document.getElementById('salesChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+        datasets: [{
+            label: 'USA',
+            data: [15, 30, 55, 65, 60, 80, 95],
+            backgroundColor: '#a91617',
+        },
+        {
+            label: 'UK',
+            data: [8, 35, 40, 60, 70, 55, 75],
+            backgroundColor: '#832122'
+        },
+        {
+            label: 'AU',
+            data: [12, 25, 45, 55, 65, 70, 60],
+            backgroundColor: '#5a2023'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
