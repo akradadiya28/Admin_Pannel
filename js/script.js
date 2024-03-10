5a2d26c9022c44df6a8ca4d0fc3045af00f107c5
akradadiya28 <akradadiya28@gmail.com>
2024-03-10 17:23:05 +0530

    calander added

--- js/script.js
+++ js/script.js
@@ -73,23 +73,10 @@ new Chart(ctx1, {
 
 });
 // calander
-// document.addEventListener('DOMContentLoaded', function () {
-//     var calendarEl = document.getElementById('calendar');
-
-//     var calendar = new FullCalendar.Calendar(calendarEl, {
-//         selectable: true,
-//         headerToolbar: {
-//             left: 'prev,next today',
-//             center: 'title',
-//             right: 'dayGridMonth,timeGridWeek,timeGridDay'
-//         },
-//         dateClick: function (info) {
-//             alert('clicked ' + info.dateStr);
-//         },
-//         select: function (info) {
-//             alert('selected ' + info.startStr + ' to ' + info.endStr);
-//         }
-//     });
-
-//     calendar.render();
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
