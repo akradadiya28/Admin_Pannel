89feac592e7b7bc4a58c0d6111b671fd73aa161a
akradadiya28 <akradadiya28@gmail.com>
2024-03-12 15:14:14 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $(".dropdown").click(function () {
         $(this).next(".dropdown-menu").slideToggle("fast");
-        $(this).addClass("active");
+        // $(this).addClass("active");
         $(this).parent("li").siblings("li").children(".dropdown-menu").slideUp("fast");
-        $(this).parent("li").siblings("li").children(".dropdown").removeClass("active")
+        // $(this).parent("li").siblings("li").children(".dropdown").removeClass("active")
     })
     $(".form-control").click(function () {
         $(this).addClass("outline");
